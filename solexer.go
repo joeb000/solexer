7983062a46a793ec76d490cbe7190ab3b660adf6
Bernitt <j.b5286@gmail.com>
2017-01-20 14:51:29 -0500

    adding json output

--- solexer.go
+++ solexer.go
@@ -3,7 +3,9 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"regexp"
@@ -13,20 +15,30 @@ import (
 )
 
 type arg struct {
-	name    string
-	argType string
+	Name    string `json:"name"`
+	ArgType string `json:"type"`
 }
 type function struct {
-	name       string
-	args       []arg
-	returnType string
+	Name       string `json:"name"`
+	Args       []arg  `json:"args"`
+	ReturnType string `json:"returnType"`
 }
 
-func main() {
+const (
+	version = "0.0.1"
+)
 
+var (
+	iow io.Writer
+)
+
+func main() {
+	iow = io.Writer(os.Stdout)
+	fmt.Fprintln(iow, "Testing testing")
 	app := cli.NewApp()
 	app.Name = "solexer"
 	app.Usage = "Lexical Analyzer for solidity files"
+	app.Version = version
 	app.Action = func(c *cli.Context) error {
 		solexerEntry(c)
 		return nil
@@ -53,7 +65,7 @@ func main() {
 			Name:  "about",
 			Usage: "More info about this project",
 			Action: func(c *cli.Context) error {
-				fmt.Println("Find out more about this project at github.com/joeb000/solexer")
+				fmt.Fprintln(iow, "Find out more about this project at github.com/joeb000/solexer")
 				return nil
 			},
 		},
@@ -65,9 +77,18 @@ func main() {
 
 func solexerEntry(c *cli.Context) {
 
+	if c.String("out") != "" {
+		file, err := os.OpenFile(c.String("out"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			fmt.Errorf("Error opening file: %v", err)
+		}
+		defer file.Close()
+		iow = io.Writer(file)
+	}
+
 	b, err := ioutil.ReadFile(c.String("input-file"))
 	if err != nil {
-		fmt.Print(err)
+		fmt.Fprint(iow, err)
 	}
 	str := string(b) // convert content to a 'string'
 	str = stripLineComments(str)
@@ -75,17 +96,23 @@ func solexerEntry(c *cli.Context) {
 	str = strings.Replace(str, "\n", " ", -1) //strip newlines
 
 	fSlice := createFuncSlice(str)
+	jsonOut, err := json.Marshal(fSlice)
+	if err != nil {
+		fmt.Errorf("Error marshalling json: %v", err)
+	}
+	fmt.Fprintln(iow, string(jsonOut))
 	for _, result := range fSlice {
-		fmt.Printf("Function: %s\nArgs: %s\nReturns: %s\n\n", result.name, result.args, result.returnType)
+		fmt.Fprintf(iow, "Function: %s\nArgs: %s\nReturns: %s\n\n", result.Name, result.Args, result.ReturnType)
 
 	}
+
 }
 
 func createFuncSlice(s string) []function {
 
 	funcRx := regexp.MustCompile(`function.*?{`)
 	strSlice := funcRx.FindAllString(s, -1)
-	fmt.Println(strSlice)
+	fmt.Fprintln(iow, strSlice)
 
 	fnslice := make([]function, len(strSlice))
 
@@ -96,7 +123,7 @@ func createFuncSlice(s string) []function {
 
 	for i, f := range strSlice {
 
-		fnslice[i].name = strings.TrimSpace(strings.Replace(nameRx.FindString(f), "(", " ", 1))
+		fnslice[i].Name = strings.TrimSpace(strings.Replace(nameRx.FindString(f), "(", " ", 1))
 		args := removeSpaceAndParens(argsRx.FindString(f))
 		argSlice := strings.SplitN(args, ",", -1)
 
@@ -105,15 +132,15 @@ func createFuncSlice(s string) []function {
 			splitArg := spaceRx.FindAllString(a, -1)
 			if len(splitArg) == 2 {
 				var anArg arg
-				anArg.argType = splitArg[0]
-				anArg.name = splitArg[1]
-				fnslice[i].args = append(fnslice[i].args, anArg)
+				anArg.ArgType = splitArg[0]
+				anArg.Name = splitArg[1]
+				fnslice[i].Args = append(fnslice[i].Args, anArg)
 			}
 		}
 
 		//find return
 		if strings.Contains(f, "returns") {
-			fnslice[i].returnType = removeSpaceAndParens(strings.Trim(retRx.FindString(f), "returns"))
+			fnslice[i].ReturnType = removeSpaceAndParens(strings.Trim(retRx.FindString(f), "returns"))
 		}
 	}
 
